5eda76e4ad76925f6d0cf352fa3df1acd1cdcd68
Benny Lisangi <benilisangi2019@gmail.com>
2022-03-04 16:17:15 -0500

    creating environment

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import flask
-
+import rando
 app = flask.Flask(__name__)
 
 # set up a separate route to serve the index.html file generated
@@ -18,7 +18,15 @@ def index():
     # NB: DO NOT add an "index.html" file in your normal templates folder
     # Flask will stop serving this React page correctly
     return flask.render_template("index.html")
-
+@bp.route("/home")
+def Home():
+    facts = [
+         "I am a man.",
+         "random fact 2",
+         "random fact 3"
+     ]
+    random_choice = random.choice(facts)
+    return flask.jsonify({"fact":random_choice})
 
 app.register_blueprint(bp)
 
